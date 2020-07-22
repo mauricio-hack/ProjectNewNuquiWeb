ec7282d7d8119b5d696540d77d35ab8fa511dad2
Mauricio <mauricio.velez.osorno.14@gmail.com>
2020-07-21 22:45:35 -0500

    se implementan nuevos componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 
+
+
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
