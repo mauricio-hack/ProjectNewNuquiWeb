57573051009064ed65ee0c94149214e53c47292c
Mauricio <mauricio.velez.osorno.14@gmail.com>
2020-07-22 00:15:08 -0500

    se actualizan nuevos modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
 
 
 
@@ -11,6 +12,7 @@ import { LoginComponent } from './components/login/login.component';
   declarations: [
     AppComponent,
     LoginComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
