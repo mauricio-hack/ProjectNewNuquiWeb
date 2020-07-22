83f3ea61a1abcc93dfca618fdb98704668d4e569
Mauricio <mauricio.velez.osorno.14@gmail.com>
2020-07-22 03:30:01 -0500

    se actualiza metodo login

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, FormControlDirective } from '@angular/forms';
 import { AuthService } from '../sercives/auth.service';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -14,14 +15,23 @@ export class LoginComponent implements OnInit {
     email: new FormControl(''),
     password: new FormControl('')
   });
-  constructor(private authsvc: AuthService) { }
+  constructor(private authsvc: AuthService, private  router: Router) { }
 
   ngOnInit(): void {
   }
 
-  onLogin(){
+  async onLogin(){
     const {email, password} = this.loginForm.value;
-    this.authsvc.login(email, password);
+    try{
+    const user = await  this.authsvc.login(email, password);
+    if (user){
+     this.router.navigate(['/home']);
+//redirec
+    }
+    }catch (error){
+      console.log(error);
+    }
+
   }
 
 }
